1a81aa3c236e6ea037e84e2e7df877af7ba12b55
AyushS1125 <ayushforfriend2005@gmail.com>
2025-02-16 10:21:27 +0000

    day 1

---
+++ demo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Corrected #include
+
+using namespace std;  // This allows us to use 'cout' directly
+
+int main() {
+    cout << "hello";  // No need to write 'std::cout' because we are using 'namespace std'
+    return 0;
+}
